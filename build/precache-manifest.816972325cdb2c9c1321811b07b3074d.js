7427ae08fe3181d74fcaf6ff6c1b483c8e7e21c5
GanesMishra <129545288+GanesMishra@users.noreply.github.com>
2023-04-01 11:57:35 +0530

    Add files via upload

---
+++ build/precache-manifest.816972325cdb2c9c1321811b07b3074d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56e8d70afe0f2a8963b9",
+    "url": "/static/css/main.06680239.chunk.css"
+  },
+  {
+    "revision": "56e8d70afe0f2a8963b9",
+    "url": "/static/js/main.56e8d70a.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "a29223be4d44925c0771",
+    "url": "/static/js/2.a29223be.chunk.js"
+  },
+  {
+    "revision": "0e85d9ea22f618c5f874",
+    "url": "/static/js/runtime~main.0e85d9ea.js"
+  },
+  {
+    "revision": "b1ae5abf1fe08770af79acafa35df0cf",
+    "url": "/index.html"
+  }
+];
